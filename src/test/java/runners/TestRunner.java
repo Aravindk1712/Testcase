f8e63edf196226841f32ac3624edf1433c64f8bc
Aravindk1712 <aravind.k1712@gmail.com>
2023-03-21 20:39:55 +0530

    added

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,24 @@
+package runners;
+
+
+
+
+
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features ="src/test/resources/functionalTests",
+		glue= {"stepDefinitions"},
+		tags="@smoke",
+			plugin = {"json:target/cucumber.json"}
+//		"io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm"
+		)
+public class TestRunner {
+	
+}
